4e36443b4074aaf594acc30edf62b5ff16fda3a0
mwalima <m.peltenburg@hotmail.com>
2018-04-26 10:09:32 +0200

    what we do page

--- resources/views/whatwedo.blade.php
+++ resources/views/whatwedo.blade.php
@@ -6,18 +6,21 @@
 <body>
 <div class="index_title">
     <h1><img src="/images/w_medium.png">hat we do</h1>
+
+<div class="setting_up">
+    <h2>Setting up laravel on a synology server</h2>
+    <p>first activate webstation on synology</p>
+    <a href="https://www.synology.com/nl-nl/knowledgebase/DSM/tutorial/Application/How_to_host_a_website_on_Synology_NAS">How to setup webstation --></a>
 </div>
-
-
-<!-- NAVIGATION MENU -->
-
+</div>
 <div class="navbar-nav navbar-inverse navbar-fixed-top">
     @include('partials.nav_menu')
 </div>
 <div class="container">
 </div>
+
+</body>
 <div id="footerwrap">
     @include('partials.footer')
 </div>
-</body>
 </html>
